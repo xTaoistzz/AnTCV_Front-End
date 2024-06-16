5815ae35da7ed0786e468d698f39db5631b60e5a
Your Name <PhuOfficial07@gmail.com>
2024-06-16 23:31:58 +0700

    Create Project Dialog Success

--- app/components/project/create.tsx
+++ app/components/project/create.tsx
@@ -1,18 +1,41 @@
+"use client"
 import React from "react";
 import Modal from "react-modal";
 import { useRouter } from "next/navigation";
-
+import { useState } from "react";
 const successMessage = "Login successful!";
 
-interface SuccessDialogProps {
+interface CreateDialogProps {
   isOpen: boolean;
   onClose: () => void;
 }
-export default function Create({
-  isOpen,
-  onClose,
-}: SuccessDialogProps) {
+export default function Create({ isOpen, onClose }: CreateDialogProps) {
+  const [project_name, setName] = useState("");
+  const [description, setDescription] = useState("");
+  const [msg, setMsg] = useState('');
   const router = useRouter();
+
+  const handleSubmit = async(e: React.FormEvent) => {
+    e.preventDefault()
+    const formData = { project_name, description }
+    try {
+      const res = await fetch(`${process.env.ORIGIN_URL}/create/project`, {
+        method:'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+        credentials : "include"
+      })
+      const data = await res.json()
+      if (data.type === "success") {
+        setMsg(data.message)
+        window.location.reload()
+      }
+    } catch (error) {
+      console.error("Error Logging In: ", error);
+    }
+  };
   return (
     <div className="">
       <Modal
@@ -20,15 +43,23 @@ export default function Create({
         onRequestClose={onClose}
         className=" bg-white h-1/2 m-20 text-center border border-black rounded-lg content-center space-y-4"
       >
-        <div className="font-extrabold text-5xl">Success</div>
-        <div className="font-bold text-xl">{successMessage}</div>
+        <div className="font-extrabold text-xl">Create New Project</div>
+        <div className="font-bold text-xl"></div>
         <div className="space-x-4">
-          <button
-            onClick={() => router.push('/project')}
-            className="bg-gray-300 p-2 w-28"
-          >
-            Close
-          </button>
+          <form onSubmit={handleSubmit} className="flex flex-col space-y-4 items-center">
+            <div className="flex flex-col">
+              <label htmlFor="project name" className="text-left">Project name</label>
+              <input onChange={(e) => setName(e.target.value)} type="text" placeholder="Enter Project name" className="border-2 border-gray-200 rounded-md focus:outline-gray-500 w-full"/>
+            </div>
+            <div className="flex flex-col">
+              <label htmlFor="description" className="text-left">Desciption</label>
+              <textarea onChange={(e) => setDescription(e.target.value)} placeholder="Description" className="border-2 border-gray-200 rounded-md focus:outline-gray-500 w-full"/>
+            </div>
+            {msg && <div className="text-green-500">{msg}</div>}
+            <button type="submit" className="text-black border-2 border-black py-2 px-4 rounded hover:bg-gray-300 ">
+              Create Project
+            </button>
+          </form>
         </div>
       </Modal>
     </div>
--- app/project/page.tsx
+++ app/project/page.tsx
@@ -1,20 +1,35 @@
+"use client"
+
 import React from "react";
 import ProjectApp from "../components/project/allproject";
 import Create from "../components/project/create";
+import { useState } from "react";
+
+
 
 const Home: React.FC = () => {
+  const [showCreate, setCreate] = useState(false);
+
+const handleCreate = () => {
+  setCreate(true);
+}
+
+const handleCloseCreate = () => {
+  setCreate(false);
+}
   return (
     <div className=" h-screen m-5 rounded-lg border border-black">
       <div className="p-10">
         <div className="text-center flex justify-between p-5">
           <div className=" text-xl content-center">Project</div>
-          <div><button className="border border-black p-3 rounded-md">+ Create Project</button></div>
+          <div><button className="border border-black p-3 rounded-md" onClick={handleCreate}>+ Create Project</button></div>
         </div>
         <div className="border-b border-black mb-7"></div>
         <div className="space-x-2">
           <ProjectApp />
         </div>
       </div>
+      <Create isOpen={showCreate} onClose={handleCloseCreate}/>
     </div>
   );
 };
