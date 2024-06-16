1d7b52358524d2caca315c6ac12b0f016cee4db4
Your Name <PhuOfficial07@gmail.com>
2024-06-16 22:35:28 +0700

    Change dotenv2

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,23 @@
+
+
+import {} from 'dotenv/config'
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     env: {
-        ORIGIN_URL : process.env.ORIGIN_URL,
+        BACK_URL : process.env.BACK_URL
     },
+    images: {
+        remotePatterns: [
+        {
+            protocol: process.env.BACK_URL_PROTOCOL,
+            hostname: process.env.BACK_DOMAIN,
+            port: process.env.BACK_URL_PORT,
+            // pathname: `/img/**`  
+        }
+        ]
+    },
+
 };
 
 export default nextConfig;
