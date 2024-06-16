6b41b70694ea57a70fca095a6b5fd7575770849a
Your Name <PhuOfficial07@gmail.com>
2024-06-16 23:00:26 +0700

    Check deploy dotenv

--- app/components/project/create.tsx
+++ app/components/project/create.tsx
@@ -1,6 +1,36 @@
-export default function Create(){
+import React from "react";
+import Modal from "react-modal";
+import { useRouter } from "next/navigation";
 
-    return (
-        <div></div>
-    )
+const successMessage = "Login successful!";
+
+interface SuccessDialogProps {
+  isOpen: boolean;
+  onClose: () => void;
+}
+export default function Create({
+  isOpen,
+  onClose,
+}: SuccessDialogProps) {
+  const router = useRouter();
+  return (
+    <div className="">
+      <Modal
+        isOpen={isOpen}
+        onRequestClose={onClose}
+        className=" bg-white h-1/2 m-20 text-center border border-black rounded-lg content-center space-y-4"
+      >
+        <div className="font-extrabold text-5xl">Success</div>
+        <div className="font-bold text-xl">{successMessage}</div>
+        <div className="space-x-4">
+          <button
+            onClick={() => router.push('/project')}
+            className="bg-gray-300 p-2 w-28"
+          >
+            Close
+          </button>
+        </div>
+      </Modal>
+    </div>
+  );
 }
